fba5d96fb6e524d1e31b6bca95375d6d586469e2
SkyHook42 <dale.oldham.projects@gmail.com>
2019-10-21 12:11:43 -0600

    mid day back up, working on Udemy 11

---
+++ src/javascript/tdd-01.js
@@ -0,0 +1,22 @@
+/*
+    Our goal is to:
+    Write a function called emailBuilder to create an email from a 
+    first and last name.
+    The email will be: first initial + last name
+    larry shumlich should create an email l.shumlich@evolveu.ca
+*/
+/*
+    Write the stub 
+    Share with your neighbors
+		There is a correct and wrong answer here
+*/
+
+
+const functions = {
+	emailBuilder: (fInit, lName) => {
+		return 'l.shumlich@evolveu.ca';
+	}
+
+};
+
+export default functions; 
---
+++ src/udemyjsdom/103/exercise1.js
@@ -0,0 +1,35 @@
+
+// Guess what answers you would get if you ran this in the
+// Javascript Console in Google Chrome. Once you have an answer  to the questions then 
+// check them by copying them and running it in the console yourself line by line 
+
+
+//Evaluate the below:
+5 + "34" //guess: 39, actual: "534" - thought the string had to be first to coerce
+5 - "4" //guess: 1, actual: 1
+10 % 5 //guess: 0, actual: 0
+5 % 10 //guess: 5, actual: 5
+"Java" + "Script" //guess: JavaScript, actual: JavaScript
+" " + " " //guess: "  ", actual: "  "
+" " + 0 //guess: " 0", actual: " 0"
+true + true //guess: true, actual: 2 - ah!
+true + false //guess: 1, actual: 1
+false + true //guess: 1, actual: 1
+false - true //guess: -1, actual: -1
+3 - 4 //guess: -1, actual: -1
+"Bob" - "bill" //guess: Nan, actual: Nan
+
+//Evaluate the below comparisons:
+5 >= 1 //guess: true, actual: true
+0 === 1 //guess: false, actual: false
+4 <= 1 //guess: false, actual: false
+1 != 1 //guess: false, actual: false
+"A" > "B" //guess: true, actual: false - ? I thought caps came in order?
+"B" < "C" //guess: false, actual: true - rule holds, uphill not downhill
+"a" > "A" //guess: false, actual: true - so lc before uc too
+"b" < "A" //guess: false, actual: false
+true === false //guess: false, actual: false
+true != true //guess: false, actual: false
+
+// Make the string: "Hi There! It's "sunny" out" by using the + sign:
+var list = [3,2,1,'d','e','r','f','VJJJ','K','D','S','A'];
