b664fdcc527cc6cbf7f4c6d6412c2a380b01970d
SkyHook42 <dale.oldham.projects@gmail.com>
2019-10-22 13:15:40 -0600

    lunch bu - Udemy 111 complete

--- src/udemyjsdom/exerciseplatform.js
+++ src/udemyjsdom/exerciseplatform.js
@@ -1,4 +1,16 @@
-var firstname = "Billy";
-if(firstname!==="Billy");{
-	alert('Hi Billy')
-};
+function multiply(a,b){
+	if (a>10||b>10){
+		return "that's too hard";
+	} else {
+			return a*b;
+	}
+}
+
+console.log(multiply(7,10));
+
+
+// sing("oopoink");
+// function song(){
+// 	console.log("ahhhhh");
+// 	console.log("teeeee");
+// }
