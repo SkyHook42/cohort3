a861a78c2a518478417b11c3a9e63fa847230c9b
SkyHook42 <dale.oldham.projects@gmail.com>
2019-10-25 16:24:42 -0600

    PM test lecture exercise

--- src/javascript/10_25_tdd.js
+++ src/javascript/10_25_tdd.js
@@ -7,6 +7,7 @@ const functions = {
 	hello: ()=>{
 		console.log("hello from 10_25_tdd.js");
 	},
+	
 	sum: (a,b)=>{
 		return 5;
 	},
---
+++ src/javascript/10_25_tddmain.js
@@ -0,0 +1,9 @@
+import functions from "./10_25_tdd.js";
+
+console.log("Hello world from 10_25tddmain.js");
+console.log(idDoIt);
+
+idDoIt.addEventListener("click", () => {
+console.log("Inside Event Listener");
+console.log("Number1: ", idNum1.value);		
+});
