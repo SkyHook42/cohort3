e300fa028aded5d8598659969fbe42fd5d3a541b
SkyHook42 <dale.oldham.projects@gmail.com>
2019-10-28 14:24:19 -0600

    Udemy DOM127 complete

--- src/udemyjsdom/127/script.js
+++ src/udemyjsdom/127/script.js
@@ -2,55 +2,58 @@ let enterButt = document.getElementById("enter");
 let input = document.getElementById("userinput");
 let ul = document.querySelector("ul");
 
-// let liDel = document.querySelector("li");
-const addDelButt=function(){
-	console.log("invoked addDelButt");
-}
 const addLiAfterClick=function(){
+	// check input isn't blank
 	if (inputLength() > 0){
 		createListElement();
 	}
 }
+
 const addLiAfterKey=function(event){
+	// check for enter key after field input
 	if (inputLength() > 0 && event.keyCode === 13) {
 		createListElement();
 	}
 }
+
 const createListElement=function(){
 	// adds unordered list item, delete button, both id
 	let liDesc=document.createElement("li");
 	let liButt=document.createElement("button");
 	liDesc.appendChild(liButt);
 	liDesc.appendChild(document.createTextNode(input.value));
-	liDesc.id="li"+input.value;
 	liButt.innerHTML="delete";
-	liButt.id="butt"+input.value;
 	ul.appendChild(liDesc);
 	input.value = "";
+	input.focus();
 }
-const delLiAfterClick=function(buttTarget){
-	return ul.removeChild(buttTarget);
+
+const delLiAfterClick=function(evElement){
+	// delete list item
+	return evElement.parentNode.remove();
 }
+
 const inputLength=function(){
+	// get field input length
 	return input.value.length;
 }
-const liModChoice=function(){
-	let eTarget=document.getElementById(event.target.id);
-	let liTarget=eTarget.parentNode;
-	// failure to handle the li event outside the button
-	// if (target id starts with "li"){
-		// call togClass(which target)
-	// } else {
-		delLiAfterClick(liTarget);
-	// }
+
+const liModChoice=function(ev){
+	// choose which li element child was selected
+	if (ev.target.tagName==="LI"){
+		return togClass(ev.target);
+	}
+	return delLiAfterClick(ev.target);
 }
-const togClass=function(){
-	console.log("invoked toggleDone");
-	// 1. If you click on the list item, it toggles the .done  class on and off.
-	// 	return eTarget.setAttribute("class","done");
+
+const togClass=function(evElement){
+	// toggle strikethrough appearance with class
+	if(evElement.className==="done"){
+		return evElement.removeAttribute("class");
+	}
+	return evElement.setAttribute("class","done");
 }
 
 enterButt.addEventListener("click", addLiAfterClick);
 input.addEventListener("keypress", addLiAfterKey);
 ul.addEventListener("click", liModChoice);
-// ul.addEventListener("click", doneLiAfterClick);
