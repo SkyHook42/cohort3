11c010bf1bf2a92af85860a52e59945eb6aa12f2
SkyHook42 <dale.oldham.projects@gmail.com>
2019-10-28 16:56:12 -0600

    Udemy DOM 130 in progress

--- src/udemyjsdom/130/script.js
+++ src/udemyjsdom/130/script.js
@@ -1,30 +1,29 @@
 // var button = document.getElementById("enter");
 // var input = document.getElementById("userinput");
 // var ul = document.querySelector("ul");
+
 let css = document.querySelector("h3");
 let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
-let body=document.getElementById("gradient");
+let body = document.getElementById("gradient");
 
-// console.log(css);
-// console.log(color1);
-// console.log(color2);
-// console.log(body);
+// body.style.background="green";
+//how to set node value
+color1.value="#ff0000";
+color2.value="#ffff00";
 
-color1.addEventListener("input", function(){
-	// console.log(color1.value);
+function setGradient(){
 	body.style.background=
-	"linear-gradient(to right,",
-	+color1.value
-	+","
-	+color2.value
-	+")";
+		"linear-gradient(to right,"
+		+ color1.value
+		+ ","
+		+ color2.value
+		+ ")";
+		css.textContent=body.style.background + ";"
+}
 
-})
-color2.addEventListener("input", function(){
-	console.log(color2.value);
-})
-
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
 
 // function inputLength() {
 // 	return input.value.length;
