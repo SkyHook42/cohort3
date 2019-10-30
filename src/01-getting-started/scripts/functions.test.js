770b277a8030e5289482fac63758a71ab4f88f40
SkyHook42 <dale.oldham.projects@gmail.com>
2019-10-30 12:15:54 -0600

    2019-10-30 partial TDD exercise

--- src/01-getting-started/scripts/functions.test.js
+++ src/01-getting-started/scripts/functions.test.js
@@ -1,5 +1,29 @@
 import functions from './functions'
 
+// Exercise - Canadian Taxes
+test('tax calculator', () => {
+  let income=300000;
+  // let income=[0,1000,47630,95259,147667,210371,300000];
+  // let tax=[0,150,7144.5,16908.445,30534.525,48718.685,78296.s255]
+  expect(functions.helloTaxWorld(income)).toEqual(78296.255);
+  // expect(functions.helloTaxWorld(income)).toEqual(7637.2);
+  // let i=0;
+  // do {
+  //   console.log("i = ",i,income[i],tax[i]);
+  //   console.log(expect(functions.helloTaxWorld(income[i])).toEqual(tax[i]));
+  //   expect(functions.helloTaxWorld(income[i])).toEqual(tax[i]);
+  //   i++;
+  // }
+  // while (i< income.length);
+});
+
+// Exercise - Calculator
+
+// Exercise - Basic Syntax
+
+// Exercise - Enhancement
+
+// Exercise - Working with HTML input
 test('Check the sizes', () => {
     expect(functions.size(-1)).toBe("negative"); // Consider the edge cases
     expect(functions.size(0)).toBe("small");
