11cadab81c110afdaa353cea19e34055a8ffee5e
skyhook42 <dale.oldham.projects@gmail.com>
2019-11-28 15:08:30 -0700

    complete - C100 Basic Syntax

--- src/javascript/syntax.js
+++ src/javascript/syntax.js
@@ -1,41 +1,96 @@
-// arrays
-// - add to the front
-// - add to the end
-// update values
-// loops 
-// - for
-//- for/in
-//-  while
-// - do while
-// - forEach (with array and function)
-// Objects / Dictionaries
-// - declare object
-//-  lookup key to retrieve value
 const syntax = {
-	fDefAtt: (pTest) => {
-		switch (typeof pTest) {
-			case "number":
-				return "I'm a number.";
-			case "string":
-				return "I'm a string.";
-			case "boolean":
-				return "I'm a boolean.";
-			case "object":
-				if (Array.isArray(pTest)) {
-					return "I'm an array.";
-				}
-				return "I'm an object.";
-			case "undefined":
-				return "I'm an undefined.";
-			default:
-				return "";
-		};
-	}
-	// sample if / else
-	// functions
-	// - parameters
-	// - returns
+  fDefAtt: (pTest) => {
+    switch (typeof pTest) {
+      case "number":
+        return "I'm a number.";
+      case "string":
+        return "I'm a string.";
+      case "boolean":
+        return "I'm a boolean.";
+      case "object":
+        if (Array.isArray(pTest)) {
+          return "I'm an array.";
+        }
+        return "I'm an object.";
+      case "undefined":
+        return "I'm an undefined.";
+      default:
+        return "";
+    };
+  },
 
+  // sample if / else
+  fIfElse: (pFruit) => {
+    let sFruitOut = "";
+    if (pFruit == "Apple") {
+      return sFruitOut = "round"
+    } else {
+      return sFruitOut = "does it matter?"
+    };
+  },
+
+  // functions
+  fFuncTest: (pParIn) => {
+    // - parameters
+    let pParOut = pParIn + " returned!"
+    // - returns
+    return pParOut;
+  },
+
+  // arrays
+  fFuncFBUpDate: (arrIn) => {
+    // - add to the front
+    arrIn.unshift(17);
+    // - add to the end
+    arrIn.push(42);
+    // update values
+    arrIn[3] = arrIn[3] + 4;
+    return arrIn;
+  },
+
+  // loops 
+  fLoopy: (tallyHo) => {
+    // - for
+    let i = 0;
+    let dogWalk = 0;
+    for (i; i <= 13; i++) {
+      dogWalk = dogWalk + 1; //i= 14, no correction
+    }
+
+    // - for/in
+    for (const property in tallyHo) {
+      dogWalk = dogWalk + tallyHo[property];
+    }
+    dogWalk = dogWalk - 6;//i= 20, expected correction
+
+    // -  while
+    while (i <= 99) {
+      dogWalk = dogWalk + 1;
+      i++;
+    }
+    dogWalk = dogWalk - 86;//i = 100, expected correction
+
+    // - do while
+    do {
+      dogWalk = dogWalk + 1;
+    } while (dogWalk < 100);
+    dogWalk = dogWalk - 86;//i = 100, expected correction
+
+    // while (i=)
+    while (dogWalk > 14) {
+      dogWalk -= 1; //dogWalk = 14, no correction
+    }
+    return dogWalk;
+  },
+
+  // - forEach (with array and function)
+  fFELoop: (arrIn) => {
+    let arrOut = [];
+    arrIn.forEach(item =>
+      // double each element
+      arrOut.push(item * 2));
+    return arrOut;
+  },
 }
 
 export default syntax;
--- src/javascript/syntax.test.js
+++ src/javascript/syntax.test.js
@@ -4,7 +4,13 @@ test("syntax test", () => {
   expect(syntax.fDefAtt(42)).toEqual("I'm a number.");
   expect(syntax.fDefAtt("Howdy! the vacuum bag is hot today.")).toEqual("I'm a string.");
   expect(syntax.fDefAtt(true)).toEqual("I'm a boolean.");
-	expect(syntax.fDefAtt({Dave: "not here"})).toEqual("I'm an object.");
-  expect(syntax.fDefAtt([1,2,3,4])).toEqual("I'm an array.");
+  expect(syntax.fDefAtt({ Dave: "not here" })).toEqual("I'm an object.");
+  expect(syntax.fDefAtt([1, 2, 3, 4])).toEqual("I'm an array.");
   expect(syntax.fDefAtt()).toEqual("I'm an undefined.");
+  expect(syntax.fIfElse("Apple")).toEqual("round");
+  expect(syntax.fIfElse("Banana")).toEqual("does it matter?");
+  expect(syntax.fFuncTest("Banana")).toEqual("Banana returned!");
+  expect(syntax.fFuncFBUpDate([1, 2, 3, 4, 5])).toEqual([17, 1, 2, 7, 4, 5, 42]);
+  expect(syntax.fLoopy({ a: 1, b: 2, c: 3 })).toEqual(14);
+  expect(syntax.fFELoop([1, 2, 3, 4, 5])).toEqual([2,4,6,8,10]);
 });
