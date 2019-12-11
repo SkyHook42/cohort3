0b3229302eded19f25a6853cf14e78a11342db90
skyhook42 <dale.oldham.projects@gmail.com>
2019-12-11 12:11:43 -0700

    C140A components - nav icons added

--- react-02/src/App.js
+++ react-02/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
+import heart from './graphics/heart.svg';
+import recycle from './graphics/recycle.svg';
+import percentage from './graphics/percentage.svg';
+import cross from './graphics/cross.svg';
+import foot from './graphics/foot.svg';
+import biohazard from './graphics/biohazard.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <div className="cNav">
+          <img src={heart} className="cNavSpin" alt="heart" />
+          <img src={recycle} className="cNavSpin" alt="recycle" />
+          <img src={percentage} className="cNavSpin" alt="percentage" />
+          <img src={cross} className="cNavSpin" alt="cross" />
+          <img src={foot} className="cNavSpin" alt="foot" />
+          <img src={biohazard} className="cNavSpin" alt="biohazard" />
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
