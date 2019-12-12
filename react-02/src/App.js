d752b196927eedf006906937a19f75dc6ece1b2d
skyhook42 <dale.oldham.projects@gmail.com>
2019-12-12 14:09:50 -0700

    C140A components - nav bar location fixed

--- react-02/src/App.js
+++ react-02/src/App.js
@@ -8,33 +8,43 @@ import cross from './graphics/cross.svg';
 import foot from './graphics/foot.svg';
 import biohazard from './graphics/biohazard.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div id="iNavBar" className="cNav">
-          <img src={heart} className="cNavSpin" alt="heart" />
-          <img src={recycle} className="cNavSpin" alt="recycle" />
-          <img src={percentage} className="cNavSpin" alt="percentage" />
-          <img src={cross} className="cNavSpin" alt="cross" />
-          <img src={foot} className="cNavSpin" alt="foot" />
-          <img src={biohazard} className="cNavSpin" alt="biohazard" />
-        </div>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+  }
+
+  handleClick = e => {
+    console.log(e.target.alt)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <navDiv id="iNavBar" className="cNav">
+          <img src={heart} className="cNavSpin" alt="heart" onClick={this.handleClick} />
+          <img src={recycle} className="cNavSpin" alt="recycle" onClick={this.handleClick} />
+          <img src={percentage} className="cNavSpin" alt="percentage" onClick={this.handleClick} />
+          <img src={cross} className="cNavSpin" alt="cross" onClick={this.handleClick} />
+          <img src={foot} className="cNavSpin" alt="foot" onClick={this.handleClick} />
+          <img src={biohazard} className="cNavSpin" alt="biohazard" onClick={this.handleClick} />
+        </navDiv>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
