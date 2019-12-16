e805395db41940e93ce78752804f5a2d93721fca
skyhook42 <dale.oldham.projects@gmail.com>
2019-12-16 12:58:31 -0700

    C140B navigation complete

--- react-02/src/App.js
+++ react-02/src/App.js
@@ -3,49 +3,62 @@ import Game from './components/Game.js';
 import './App.css';
 import logo from './logo.svg';
 import gHeart from './graphics/heart.svg';
-import gGame from './graphics/recycle.svg';
-import percentage from './graphics/percentage.svg';
-import cross from './graphics/cross.svg';
-import foot from './graphics/foot.svg';
-import biohazard from './graphics/biohazard.svg';
+import gRecycle from './graphics/recycle.svg';
+import gPercentage from './graphics/percentage.svg';
+import gCross from './graphics/cross.svg';
+import gFoot from './graphics/foot.svg';
+import gBiohazard from './graphics/biohazard.svg';
 
 class App extends React.Component {
   constructor() {
     super()
+    this.state = {
+      activePage: "heart"
+    }
   }
 
-  handleClick = e => {
-    // console.log(e.target.alt)
-    // let eTarget = <tempDiv> Icon selected = {e.target.alt}</tempDiv>
+  navClick = e => {
+    this.setState({ activePage: e.target.alt })
   }
 
   render() {
-    // var eTarget;
+    let pageSelect;
+    switch (this.state.activePage) {
+      case "heart":
+        pageSelect =
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+          </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+          </a>
+          </header>
+        break;
+      case "recycle":
+        pageSelect = <Game />
+        break;
+      default:
+        console.log("you blew a switch");
+    }
+
     return (
       <div className="App">
         <div id="iNavBar" className="cNav">
-          <img src={gHeart} className="cNavSpin" alt="heart" onClick={this.handleClick} />
-          <img src={gGame} className="cNavSpin" alt="recycle" onClick={this.handleClick} />
-          <img src={percentage} className="cNavSpin" alt="percentage" onClick={this.handleClick} />
-          <img src={cross} className="cNavSpin" alt="cross" onClick={this.handleClick} />
-          <img src={foot} className="cNavSpin" alt="foot" onClick={this.handleClick} />
-          <img src={biohazard} className="cNavSpin" alt="biohazard" onClick={this.handleClick} />
+          <img src={gHeart} className="cNavSpin" alt="heart" onClick={this.navClick} />
+          <img src={gRecycle} className="cNavSpin" alt="recycle" onClick={this.navClick} />
+          <img src={gPercentage} className="cNavSpin" alt="percentage" onClick={this.navClick} />
+          <img src={gCross} className="cNavSpin" alt="cross" onClick={this.navClick} />
+          <img src={gFoot} className="cNavSpin" alt="foot" onClick={this.navClick} />
+          <img src={gBiohazard} className="cNavSpin" alt="biohazard" onClick={this.navClick} />
         </div>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <Game />
+        {pageSelect}
       </div>
     );
   }
