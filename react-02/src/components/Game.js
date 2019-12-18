f8decb5630c639e73bb6f1ddba975bf584bbfcd1
skyhook42 <dale.oldham.projects@gmail.com>
2019-12-18 13:58:28 -0700

    C140B TTT user turns ready

--- react-02/src/components/Game.js
+++ react-02/src/components/Game.js
@@ -1,24 +1,12 @@
 import React from 'react';
 import '../styleSheets/Game.css';
 
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.setState({ value: 'X' })}
-      >
-        {this.state.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
@@ -26,11 +14,29 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    // squares[i] = 'X';
+    // this.setState({squares: squares});
+
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
