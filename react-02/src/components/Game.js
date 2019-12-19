7451c9f0f43203d1789307d52da4781e7dd727b2
skyhook42 <dale.oldham.projects@gmail.com>
2019-12-19 10:22:08 -0700

    C140B TTT winner annunciate complete

--- react-02/src/components/Game.js
+++ react-02/src/components/Game.js
@@ -20,9 +20,9 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    // squares[i] = 'X';
-    // this.setState({squares: squares});
-
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -40,7 +40,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div>
         <div className="status">{status}</div>
