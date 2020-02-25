a0f3934b30609196015cabaa1de5dcb71ede2f0d
skyhook42 <dale.oldham.projects@gmail.com>
2020-02-25 09:12:58 -0700

    data import complete

--- PostgreSQL/tablepop.sql
+++ PostgreSQL/tablepop.sql
@@ -3,10 +3,5 @@ COPY incident FROM '/home/skyhook/code/cohort3/PostgreSQL/tableincident.csv' DEL
 COPY officer FROM '/home/skyhook/code/cohort3/PostgreSQL/tableofficer.csv' DELIMITER ',';
 COPY precinct FROM '/home/skyhook/code/cohort3/PostgreSQL/tableprecinct.csv' DELIMITER ',';
 COPY suspect FROM '/home/skyhook/code/cohort3/PostgreSQL/tablesuspect.csv' DELIMITER ',';
-
--- CREATE TABLE "crime" (
---   "id" char(255) PRIMARY KEY,
---   "short_name" char(255),
---   "desc" varchar,
---   "invoked" timestamp
--- );
+COPY public.incident_crime FROM '/home/skyhook/code/cohort3/PostgreSQL/tableincident_crime.csv' DELIMITER ',';
+COPY public.incident_suspect FROM '/home/skyhook/code/cohort3/PostgreSQL/tableincident_suspect.csv' DELIMITER ',';
